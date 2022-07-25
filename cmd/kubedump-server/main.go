dd26cf294855b39357a10eb5eeafc5f75ad5844d
joshmeranda <joshmeranda@gmail.com>
2022-07-25 05:37:44 -0400

    create flat compressed tar archive

--- cmd/kubedump-server/main.go
+++ cmd/kubedump-server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"io"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -10,9 +13,15 @@ import (
 	"kubedump/pkg/collector"
 	"kubedump/pkg/filter"
 	"net/http"
+	"os"
+	"path"
 	"strconv"
 	"sync"
 	"time"
+)
+
+const (
+	ParentPath = "/var/lib/kubedump"
 )
 
 func errorResponse(w http.ResponseWriter, message string, statusCode int) {
@@ -42,6 +51,60 @@ func queryFloat64OrDefault(value string, defaultValue float64) (float64, error) 
 
 func durationFromSeconds(s float64) time.Duration {
 	return time.Duration(s * float64(time.Second) * float64(time.Millisecond))
+}
+
+func archiveTree(dir string, writer *tar.Writer) error {
+	entries, err := os.ReadDir(dir)
+
+	if err != nil {
+		return fmt.Errorf("could not read directory '%s': %w", dir, err)
+	}
+
+	for _, entry := range entries {
+		entryPath := path.Join(dir, entry.Name())
+
+		logrus.Infof("looking at entry '%s'", entryPath)
+
+		if entry.IsDir() {
+			if err = archiveTree(entryPath, writer); err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		file, err := os.Open(entryPath)
+
+		if err != nil {
+			return fmt.Errorf("could not open file at '%s': %w", entryPath, err)
+		}
+
+		info, err := entry.Info()
+
+		if err != nil {
+			return fmt.Errorf("could not get file info for file '%s': %w", entryPath, err)
+		}
+
+		hdr, err := tar.FileInfoHeader(info, entry.Name())
+
+		if err != nil {
+			return fmt.Errorf("could not construct header for file '%s': %w", entryPath, err)
+		}
+
+		err = writer.WriteHeader(hdr)
+
+		if err != nil {
+			return fmt.Errorf("could not write header for file '%s': %w", entryPath, err)
+		}
+
+		_, err = io.Copy(writer, file)
+
+		if err != nil {
+			return fmt.Errorf("could not copy file '%s' to archive: %w", entryPath, err)
+		}
+	}
+
+	return nil
 }
 
 type KubedumpHandler struct {
@@ -79,10 +142,49 @@ func (handler *KubedumpHandler) handleHealth(w http.ResponseWriter, _ *http.Requ
 func (handler *KubedumpHandler) handleTar(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		// todo: generate tar
+		file, err := os.Create("/tmp/archive.tar.gz")
+
+		if err != nil {
+			errorResponse(w, fmt.Sprintf("could not open temporary archive file: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		// todo: support better speed / compression
+		compressor := gzip.NewWriter(file)
+		archiver := tar.NewWriter(compressor)
+
+		err = archiveTree(ParentPath, archiver)
+
+		if err != nil {
+			errorResponse(w, fmt.Sprintf("could not archive '%s': %s", ParentPath, err), http.StatusInternalServerError)
+			return
+		}
+
+		// flush archive writes
+		if err := archiver.Close(); err != nil {
+			errorResponse(w, fmt.Sprintf("could not close arhive writer: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		if err := compressor.Close(); err != nil {
+			errorResponse(w, fmt.Sprintf("could not close arhive writer: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = file.Seek(0, io.SeekStart)
+
+		if err != nil {
+			errorResponse(w, fmt.Sprintf("could not seek start of archive: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = io.Copy(w, file)
+
+		if err != nil {
+			errorResponse(w, fmt.Sprintf("could not copy archive to response: %s", err), http.StatusInternalServerError)
+		}
 	default:
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte(fmt.Sprintf("unsupported method '%s'", r.Method)))
+		errorResponse(w, fmt.Sprintf("method is not supported: %s", r.Method), http.StatusMethodNotAllowed)
 	}
 }
 
@@ -116,21 +218,19 @@ func (handler *KubedumpHandler) handleStart(w http.ResponseWriter, r *http.Reque
 			errorResponse(w, fmt.Sprintf("could not parse query filter as filter: %s", err), http.StatusBadRequest)
 		}
 
-		parentPath := "/var/log/kubedump"
-
 		opts := collector.ClusterCollectorOptions{
-			ParentPath: parentPath,
+			ParentPath: ParentPath,
 			Filter:     f,
 			NamespaceCollectorOptions: collector.NamespaceCollectorOptions{
-				ParentPath: parentPath,
+				ParentPath: ParentPath,
 				Filter:     f,
 				PodCollectorOptions: collector.PodCollectorOptions{
-					ParentPath:          parentPath,
+					ParentPath:          ParentPath,
 					LogInterval:         durationFromSeconds(podLogInterval),
 					DescriptionInterval: durationFromSeconds(podDescInterval),
 				},
 				JobCollectorOptions: collector.JobCollectorOptions{
-					ParentPath:          parentPath,
+					ParentPath:          ParentPath,
 					DescriptionInterval: durationFromSeconds(jobDescInterval),
 				},
 			},
