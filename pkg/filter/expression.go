90db88063e7b41a743eff0d2e28938558cb84d39
joshmeranda <joshua.meranda@suse.com>
2022-09-13 20:30:39 -0400

    implement label filter

--- pkg/filter/expression.go
+++ pkg/filter/expression.go
@@ -115,3 +115,33 @@ func (expr namespaceExpression) checkObject(obj apismetav1.Object) bool {
 		return false
 	}
 }
+
+type labelExpression struct {
+	labelPatterns map[string]string
+}
+
+func (expr labelExpression) Matches(v interface{}) bool {
+	var labels map[string]string
+
+	switch v.(type) {
+	case apicorev1.Pod:
+		labels = v.(apicorev1.Pod).Labels
+	case apibatchv1.Job:
+		labels = v.(apibatchv1.Job).Labels
+	default:
+		return false
+	}
+
+patternLoop:
+	for namePattern, valuePattern := range expr.labelPatterns {
+		for name, value := range labels {
+			if wildcard.MatchSimple(namePattern, name) && wildcard.MatchSimple(valuePattern, value) {
+				continue patternLoop
+			}
+		}
+
+		return false
+	}
+
+	return true
+}
--- pkg/filter/expression_test.go
+++ pkg/filter/expression_test.go
@@ -115,3 +115,146 @@ func TestNamespace(t *testing.T) {
 
 	assert.False(t, expr.Matches("non-object-values"))
 }
+
+func TestLabel(t *testing.T) {
+	type MapPair struct {
+		Key   string
+		Value string
+	}
+
+	var (
+		simple  = MapPair{"app", "kubedump"}
+		wcKey   = MapPair{"*-wc-key", "simple-value"}
+		wcValue = MapPair{"simple-key", "*-wc-value"}
+	)
+
+	simpleExpr := labelExpression{
+		labelPatterns: map[string]string{
+			simple.Key: simple.Value,
+		},
+	}
+
+	wcKeyExpr := labelExpression{
+		labelPatterns: map[string]string{
+			wcKey.Key: wcKey.Value,
+		},
+	}
+	wcValueExpr := labelExpression{
+		labelPatterns: map[string]string{
+			wcValue.Key: wcValue.Value,
+		},
+	}
+
+	assert.True(t, simpleExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				simple.Key: simple.Value,
+			},
+		},
+	}))
+
+	assert.False(t, simpleExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				simple.Key: "other value",
+			},
+		},
+	}))
+
+	assert.False(t, simpleExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				"other key": simple.Value,
+			},
+		},
+	}))
+
+	assert.True(t, wcKeyExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				wcKey.Key: wcKey.Value,
+			},
+		},
+	}))
+
+	assert.False(t, wcKeyExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				wcKey.Key: "other value",
+			},
+		},
+	}))
+
+	assert.False(t, wcKeyExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				"other key": wcKey.Value,
+			},
+		},
+	}))
+
+	assert.True(t, wcValueExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				wcValue.Key: wcValue.Value,
+			},
+		},
+	}))
+
+	assert.False(t, wcValueExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				wcValue.Key: "other value",
+			},
+		},
+	}))
+
+	assert.False(t, wcValueExpr.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				"other key": wcValue.Value,
+			},
+		},
+	}))
+
+	union := labelExpression{
+		labelPatterns: map[string]string{
+			simple.Key:  simple.Value,
+			wcKey.Key:   wcKey.Value,
+			wcValue.Key: wcValue.Value,
+		},
+	}
+
+	// 1 1 1
+	assert.True(t, union.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				simple.Key:  simple.Value,
+				wcKey.Key:   wcKey.Value,
+				wcValue.Key: wcValue.Value,
+			},
+		},
+	}))
+
+	// 1 0 1 (failed value)
+	assert.False(t, union.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				simple.Key:  simple.Value,
+				wcKey.Key:   "other value",
+				wcValue.Key: wcValue.Value,
+			},
+		},
+	}))
+
+	// 1 0 1 (failed key)
+	assert.False(t, union.Matches(apicorev1.Pod{
+		ObjectMeta: apismeta.ObjectMeta{
+			Labels: map[string]string{
+				simple.Key:  simple.Value,
+				"other key": wcKey.Key,
+				wcValue.Key: wcValue.Value,
+			},
+		},
+	}))
+}
