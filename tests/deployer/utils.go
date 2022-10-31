6ce95e9c4682db56dd81bee3d4bc0ee4728e8b66
joshmeranda <joshmeranda@gmail.com>
2022-10-31 00:12:49 -0400

    add / fix util functions

--- tests/deployer/utils.go
+++ tests/deployer/utils.go
@@ -1,10 +1,14 @@
 package deployer
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var runeSet = []rune("0123456789-.abcdefghijklmnop")
 
 func randomPostfix(length int) string {
+	rand.Seed(time.Now().Unix())
 	runes := make([]rune, length)
 
 	for i := range runes {
--- tests/utils.go
+++ tests/utils.go
@@ -1,12 +1,28 @@
 package tests
 
 import (
+	"fmt"
+	"github.com/gobwas/glob"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	apisappsv1 "k8s.io/api/apps/v1"
+	apisbatchv1 "k8s.io/api/batch/v1"
+	apiscorev1 "k8s.io/api/core/v1"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
+	"path"
 	"path/filepath"
+	"sigs.k8s.io/yaml"
 	"strings"
 	"testing"
 )
+
+// exists checks if a file exists.
+func exists(filePath string) bool {
+	_, err := os.Stat(filePath)
+
+	return !os.IsNotExist(err)
+}
 
 // displayTree will display the entire directory structure pointed to by dir.
 func displayTree(t *testing.T, dir string) {
@@ -33,7 +49,7 @@ func copyTree(t *testing.T, target string, destination string) {
 	target, _ = filepath.Abs(target)
 	destination, _ = filepath.Abs(destination)
 
-	if _, err := os.Stat(destination); err == nil {
+	if exists(destination) {
 		t.Errorf("directory '%s' already exists, not copying", destination)
 		return
 	}
@@ -63,4 +79,70 @@ func copyTree(t *testing.T, target string, destination string) {
 	if err != nil {
 		t.Logf("error walking directory")
 	}
+}
+
+// unmarshalFile will attempt to marshal teh file at the given path into the given object.
+func unmarshalFile(fileName string, obj interface{}) error {
+	data, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(data, obj)
+
+	return err
+}
+
+// findGlobsIn will find all top level files in the given directory that match the given pattern.
+func findGlobsIn(parent string, pattern glob.Glob) ([]string, error) {
+	var found []string
+
+	children, err := os.ReadDir(parent)
+	if err != nil {
+		return nil, fmt.Errorf("could not read directory '%s': %w", parent, err)
+	}
+
+	for _, child := range children {
+		if pattern.Match(child.Name()) {
+			found = append(found, path.Join(parent, child.Name()))
+		}
+	}
+
+	return found, nil
+}
+
+// assertResourceFile will assert if the expected iven object matches the file as stored to the filesystem.
+func assertResourceFile(t *testing.T, kind string, fileName string, obj apismetav1.Object) {
+	var fsObj apismetav1.ObjectMeta
+	var err error
+
+	switch kind {
+	case "Pod":
+		var pod apiscorev1.Pod
+		err = unmarshalFile(fileName, &pod)
+		fsObj = pod.ObjectMeta
+	case "Job":
+		var job apisbatchv1.Job
+		err = unmarshalFile(fileName, &job)
+		fsObj = job.ObjectMeta
+	case "Deployment":
+		var deployment apisappsv1.Deployment
+		err = unmarshalFile(fileName, &deployment)
+		fsObj = deployment.ObjectMeta
+	default:
+		t.Errorf("unsupported kind '%s' encountered", kind)
+	}
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, obj.GetName(), fsObj.GetName())
+	assert.Equal(t, obj.GetNamespace(), fsObj.GetNamespace())
+}
+
+func assertLinkGlob(t *testing.T, parent string, pattern glob.Glob) {
+	found, err := findGlobsIn(parent, pattern)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(found))
 }
